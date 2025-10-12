5a7603d202ea2b4ace6c6379ed7b9023dcc641fb
keviniacoUAI <kevinclaudio.iacovantuono@alumnos.uai.edu.ar>
2025-10-12 02:57:39 -0300

    fix: vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ Reemplazá "alquileres-lite" por el NOMBRE EXACTO de tu repo
 export default defineConfig({
   plugins: [react()],
-  base: '/alquileres-lite/',
+  base: '/alquileres-lite/', // <-- nombre exacto de TU repo
 })
